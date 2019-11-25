a98d17834f3a316246bacf6b9c30f36d8db50197
tyuiop <ttdys108@163.com>
2019-11-25 15:31:29 +0800

    version 1.0

---
+++ src/main/java/com/ttdys108/commons/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.ttdys108.commons.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    SYSTEM_ERROR("999999", "system error"),
+    ILLEGAL_ARGUMENT("100000", "illegal argument"),
+
+    ;
+
+
+    private ErrorCode(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+    private String code;
+    private String msg;
+
+
+}
